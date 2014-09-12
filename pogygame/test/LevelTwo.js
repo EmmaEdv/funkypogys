3636bf47725687bd6e17f47b90bb5ae9005e3e88
Emma Edvardsson <emmed608@student.liu.se>
2014-09-12 15:44:43 +0200

    Removed a couple of files hähä

--- pogygame/test/LevelTwo.js
+++ pogygame/test/LevelTwo.js
@@ -1,13 +1,14 @@
-LevelOne = function(game){
+LevelTwo = function(game){
 	this.game = game;
 	this.sprite = null;
 	this.map = null;
 	this.layer = null;
 };
 
-LevelOne.prototype = {
+LevelTwo.prototype = {
 	preload: function(){
-		this.game.load.tilemap('map', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+		this.game.load.tilemap('map', 'assets/level2.json', null, Phaser.Tilemap.TILED_JSON);
+		this.game.load.image('tree', 'assets/tree2-final.png', 256, 256);
     this.game.load.image('platform', 'assets/platform.png', 256, 256);
     this.game.load.image('number', 'assets/number-buttons-90x90.png', 256, 256);
 
@@ -18,6 +19,7 @@ LevelOne.prototype = {
 		// Create the map
     this.map = this.game.add.tilemap('map');
     this.map.addTilesetImage('platform');
+    this.map.addTilesetImage('tree');
     this.map.addTilesetImage('number');
     this.layer = this.map.createLayer('collision');
     this.map.setCollisionBetween(1, 1000);
