9b09131d56aee2c4df2e85ab048cd3e7ead02ad7
hannes ingelhag <hannesingelhag@hotmail.com>
2014-09-18 16:44:50 +0200

    Börjat på nya level1, ett litet problem

--- pogygame/test/LevelOne.js
+++ pogygame/test/LevelOne.js
@@ -1,58 +1,38 @@
 LevelOne = function(game){
 	this.game = game;
-	this.sprite = null;
 	this.map = null;
 	this.groundLayer = null;
-	this.homeLayer = null;
-	this.startLayer = null;
-	this.coinLayer = null;
 
   this.coins = null;
   this.coinId = 51;
-
-  this.slope = null;
 };
 
 LevelOne.prototype = {
 	preload: function(){
-	  this.game.load.tilemap('map', 'assets/level00001.json', null, Phaser.Tilemap.TILED_JSON);
-	  this.game.load.image('tiles', 'assets/tiles.png', 1920, 1080);
-	  this.game.load.image('coin', 'assets/coin.png', 20, 20);
-	  this.game.load.image('arrow-button', 'assets/arrow-button.png', 224, 95);
+	  this.game.load.tilemap('map', 'assets/tilemap-level1.json', null, Phaser.Tilemap.TILED_JSON);
+	  this.game.load.image('coin', 'assets/coin.png');
+	  this.game.load.image('kenney', 'assets/kenney.png');
 	  this.game.load.image('sky', 'assets/sky2.png');
-
-	  this.game.load.spritesheet('coinz', 'assets/coin.png', 20, 20);
-	  this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+	  this.game.load.spritesheet('dude', 'assets/dude.png');
 	},
 
 	create: function(){
-		this.game.add.sprite(0,0,'sky');
+		//this.game.add.sprite(0,0,'sky');
 		// Create the map
 		this.map = this.game.add.tilemap('map');
-		this.map.addTilesetImage('tiles', 'tiles');
-		this.map.addTilesetImage('coin', 'coin');
-		this.map.addTilesetImage('arrow-button', 'arrow-button');
-		this.groundLayer = this.map.createLayer('platform');
+		this.map.addTilesetImage('kenney');
+		this.groundLayer = this.map.createLayer('Tile Layer 1');
 
-		this.map.setCollisionByExclusion([51, 8045, 8093, 8094, 8095, 8096, 8097, 8098]);
 
-		this.map.setCollision(67);
-
+		this.map.setCollision(32);
 		this.groundLayer.debug = true;
-
-		console.log("platform index " + this.map.getLayerIndex('platform'));
 
 		this.coins = this.game.add.group();
 		this.coins.enableBody = true;
-		this.map.createFromObjects('coinz', 67, 'coin', 0, true, false, this.coins);
-
-		this.slope = this.game.add.group();
-		this.slope.enableBody = true;
-		this.slope.immovable = true;
-		this.map.createFromObjects('slope',  'coin', 0, true, false, this.slope);
+		this.map.createFromObjects('coins', 67, 'coin', 0, true, false, this.coins);
 	},
 
 	update: function(){
 
 	}
-}
+};
