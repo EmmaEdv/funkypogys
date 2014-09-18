36074e7fa7752255472dfd6532aa2fcdce835d93
Emma Edvardsson <emmed608@student.liu.se>
2014-09-18 16:32:14 +0200

    Man kan klicka på en tile, men den försvinner inte :) :) :)

---
+++ pogygame/test/DigPogy.js
@@ -0,0 +1,31 @@
+DigPogy = function(game){
+	this.game = game;
+	this.digpogy = null;
+  this.nrOfDigPogys = 1;
+};
+
+DigPogy.prototype = {
+	preload: function(){
+		
+	},
+
+	create: function(){
+		
+	},
+
+	update: function(){
+		if(this.game.input.mousePointer.isDown){
+			console.log("X:" + this.game.input.mousePointer.x + ", Y: " + this.game.input.mousePointer.y);
+			//Check which tile is under the pointer :) 
+			var xPos = Math.floor(this.game.input.mousePointer.x/level.tileSize);
+			var yPos = Math.floor(this.game.input.mousePointer.y/level.tileSize);
+
+			var clickedTile = level.map.getTile(xPos, yPos, level.groundLayer);
+			if(clickedTile){
+				console.log("Clicked tile: x:" + clickedTile.x+ ", y: "+ clickedTile.y + ", index: " + clickedTile.index);
+				level.map.putTile(clickedTile, 0,0);
+				clickedTile.alpha = 0.2;
+			}
+		}
+	}
+};
