a14d77753ba777451b5c5cbb3611f455b09c9225
Emma Edvardsson <emmed608@student.liu.se>
2014-09-22 14:23:06 +0200

    Går att bygga på en grävtile

--- pogygame/test/BuildPogy.js
+++ pogygame/test/BuildPogy.js
@@ -15,17 +15,21 @@ BuildPogy.prototype = {
 	},
 
 	update: function(){
-		if(this.game.input.mousePointer.isDown){
-			//console.log("X:" + this.game.input.mousePointer.x + ", Y: " + this.game.input.mousePointer.y);
+		if(this.game.input.mousePointer.isDown){	
 			//x and y positions in tile units:
-			var xPos = Math.floor(this.game.input.mousePointer.x/level.tileSize);
+			var xPos = Math.floor((this.game.input.mousePointer.x+ this.game.camera.x)/level.tileSize);
 			var yPos = Math.floor(this.game.input.mousePointer.y/level.tileSize);
-
 			var clickedTile = level.map.getTile(xPos, yPos);
-			//console.log("Build!! "+clickedTile);
+			
 			if(!clickedTile && this.active){
 				//Byt ut första parametern till en byggtile :)
-				level.map.putTile(34, xPos, yPos);
+				level.map.putTile(136, xPos, yPos);
+			}
+			//Om klickad tile är en som redan är grävd
+			else if(clickedTile && this.active){
+				if(clickedTile.index==9){
+					level.map.replace(clickedTile.index, 136, xPos, yPos, 1, 1);
+				}
 			}
 		}
 	}
